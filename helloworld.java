8e95ab8105c92334bd2669f0cb73178dcd19438d
lhafele <121243380+lhafele@users.noreply.github.com>
2022-12-22 15:58:16 +0100

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,7 @@
 public class Main {
   public static void main (String[] args) {
-    System.out.println("HELLO WORLD");
+    while(true) {
+         System.out.println("HELLO WORLD");
+    }
   }
 }
